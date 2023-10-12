54eea4a85bba4a865c3469a8481f5e24802db55c
Diego Cambronero <dcambron@checkpoint.com>
2023-10-12 15:34:21 -0600

    change message

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+const http = require('http');
 
-app.get('/', (req, res) => res.send('Hello World Diego Cambronero viva Check Point!'));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<!DOCTYPE html>');
+  res.write('<html>');
+  res.write('<head>');
+  res.write('<title>Página sobre el amor</title>');
+  res.write('</head>');
+  res.write('<body>');
+  res.write('<h1>El amor es hermoso</h1>');
+  res.write('<p>El amor es un sentimiento maravilloso que une a las personas y hace que el mundo sea un lugar mejor.</p>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+});
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Servidor web en ejecución en el puerto ${port}`);
+});

2d5e4fde480f6ccad6af301410720b1a9a104ff9
Diego Cambronero <dcambron@checkpoint.com>
2023-10-12 15:29:56 -0600

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Diego Cambronero!'));
+app.get('/', (req, res) => res.send('Hello World Diego Cambronero viva Check Point!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
